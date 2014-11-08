55f78b7750f4b62002a620db33e56229e925882c
Leonardo Marques <leommarques7@gmail.com>
2014-11-07 22:10:33 -0200

    imagem do botao animais clicado

--- app/src/main/java/com/leonardo/fonohu/MenuPrincipal.java
+++ app/src/main/java/com/leonardo/fonohu/MenuPrincipal.java
@@ -1,15 +1,21 @@
 package com.leonardo.fonohu;
 
 import android.app.Fragment;
+import android.widget.ImageView;
 
 import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EFragment;
+import org.androidannotations.annotations.ViewById;
 
 @EFragment(R.layout.frag_menu_principal)
 public class MenuPrincipal extends Fragment {
 
+    @ViewById
+    ImageView btnAnimais;
+
     @Click
     void btnAnimais() {
+        btnAnimais.setImageResource(R.drawable.btn_animais_clicado);
         getFragmentManager().beginTransaction().replace(getId(), new ComecaAnimais_()).commit();
     }
 
