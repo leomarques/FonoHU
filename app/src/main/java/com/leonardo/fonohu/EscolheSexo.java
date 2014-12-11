2adc03758b34b9c0b22d96500ec4ac8cba2523e9
Leonardo Marques <leommarques7@gmail.com>
2014-12-10 22:36:24 -0200

    entrada

--- app/src/main/java/com/leonardo/fonohu/EscolheSexo.java
+++ app/src/main/java/com/leonardo/fonohu/EscolheSexo.java
@@ -1,9 +1,16 @@
 package com.leonardo.fonohu;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.app.Fragment;
 import android.content.SharedPreferences;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
+import android.os.Handler;
+
+import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EFragment;
 import org.androidannotations.annotations.ViewById;
@@ -12,7 +19,49 @@ import org.androidannotations.annotations.ViewById;
 public class EscolheSexo extends Fragment {
 
     @ViewById
-    ImageView menino, menina;
+    ImageView menino, menina, entrada;
+
+    @ViewById
+    RelativeLayout escolha;
+
+    @AfterViews
+    void aoCriar() {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                fade();
+            }
+        }, 4000);
+    }
+
+    private void fade() {
+        int mShortAnimationDuration = getResources().getInteger(
+                android.R.integer.config_longAnimTime);
+        // Set the content view to 0% opacity but visible, so that it is visible
+        // (but fully transparent) during the animation.
+        escolha.setAlpha(0f);
+        escolha.setVisibility(View.VISIBLE);
+
+        // Animate the content view to 100% opacity, and clear any animation
+        // listener set on the view.
+        escolha.animate()
+                .alpha(1f)
+                .setDuration(mShortAnimationDuration)
+                .setListener(null);
+
+        // Animate the loading view to 0% opacity. After the animation ends,
+        // set its visibility to GONE as an optimization step (it won't
+        // participate in layout passes, etc.)
+        entrada.animate()
+                .alpha(0f)
+                .setDuration(mShortAnimationDuration)
+                .setListener(new AnimatorListenerAdapter() {
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        entrada.setVisibility(View.GONE);
+                    }
+                });
+    }
 
     @Click
     public void menino() {
